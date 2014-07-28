568f8a8e2aa8d26d77bbe8f2ecabbd5f1e4321b9
Aaron Russo <aaron.n.russo@gmail.com>
2014-07-28 10:51:18 -0700

    rewrite for ruby 1.8.7

--- check_openresty.rb
+++ check_openresty.rb
@@ -1,8 +1,17 @@
 #!/bin/env ruby
 
-require 'rest_client'
+#require 'rubygems'
+require 'net/http'
+require 'net/https'
+require 'json'
 
-response = RestClient.get 'https://api.github.com/repos/openresty/ngx_openresty/tags'
-latest = JSON.parse(response, { :symbolize_names => true })[0][:name]
+uri = URI.parse('https://api.github.com/repos/openresty/ngx_openresty/tags')
+http = Net::HTTP.new(uri.host,uri.port)
+http.use_ssl = true
+request = Net::HTTP::Get.new(uri.request_uri)
+request.add_field('User-Agent','openresty-check-1.0')
+response = http.request(request)
+
+latest = JSON.parse(response.body, { :symbolize_names => true })[0][:name]
 
 puts latest
