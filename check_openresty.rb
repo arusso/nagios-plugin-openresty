51dcd1a09dbe8b9169ee8b43ed95eb6f9be3e5cb
Aaron Russo <arusso@berkeley.edu>
2014-08-04 17:08:31 -0700

    handle cases where server throws connection refused

--- check_openresty.rb
+++ check_openresty.rb
@@ -38,8 +38,14 @@ end
 
 # We're not installing gems or anything, just using their version class to do a
 # proper comparison of version strings
-current = Gem::Version.new(get_current(ARGV[0]))
+begin
+  current = Gem::Version.new(get_current(ARGV[0]))
+rescue Errno::ECONNREFUSED
+  puts "WARN: #{ARGV[0]} refused our connection"
+  exit 2
+end
 latest = Gem::Version.new(get_latest)
+
 
 if current > latest
   # version is newer than it should be, report an unknown error
